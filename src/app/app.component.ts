fc6157a30e8d808910003128f9508e53bd2b61e0
SztGellert <sztankgellert@gmail.com>
2023-02-26 17:02:20 +0100

    added language form bound to a model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  language = '';
+  listOfLanguages: string[] = ["English", "Spanish", "German"]
   title = 'test-app';
   memberName = "Gellert";
+  addLanguage() {
+    console.log("addlanguage")
+    this.listOfLanguages.push(this.language)
+    console.log(this.listOfLanguages)
+  }
 }
